f1ee6a4f107b472c5bb244fca0a786921a945be6
Michal <mstullich1@gmail.com>
2021-09-20 16:05:50 +0200

    kalukator

---
+++ app/src/main/java/com/example/kalkulator/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        var button1 = <findViewById>(R.id.button1)
+        var button2 = <findViewById>(R.id.button2)
+        var button3 = <findViewById>(R.id.button3)
+        var button4 = <findViewById>(R.id.button4)
+        var button5 = <findViewById>(R.id.button5)
+        var button6 = <findViewById>(R.id.button6)
+        var button7 = <findViewById>(R.id.button7)
+        var button8 = <findViewById>(R.id.button8)
+        var button9 = <findViewById>(R.id.button9)
+        var button0 = <findViewById>(R.id.button0)
+        var buttonC = <findViewById>(R.id.buttonC)
+    }
+}
